bc3655db06993d1991f5c6775a3e4108e9a7d8da
tmccoid.tech <coid.tpmc@gmail.com>
2025-05-14 15:12:15 -0700

    Modified  i18n for data-i18n-* based  text localization

--- module/i18n.js
+++ module/i18n.js
@@ -16,18 +16,28 @@
  */
 
 var i18n = {
-  updateString(string) {
-    let re = new RegExp(this.keyPrefix + "(.+?)__", "g");
-    return string.replace(re, (matched) => {
+
+  i18nAttrRegex: /^data-i18n-(?<target>.*)/,
+
+  propertyMapping: new Map([
+    ["textcontent", "textContent"]
+  ]),
+
+  getTranslation(placeholder) {
+    const prefixRegex = new RegExp(this.keyPrefix + "(.+?)__", "g");
+    
+    return placeholder.replace(prefixRegex, (matched) => {
       const key = matched.slice(this.keyPrefix.length, -2);
-      let rv = this.extension
+
+      const result = this.extension
         ? this.extension.localeData.localizeMessage(key)
         : messenger.i18n.getMessage(key);
-      return rv || matched;
+
+      return result || matched;
     });
   },
 
-  updateSubtreeSet(sourceDocument, node, selector, update) {
+  updateSubtreeSet (sourceDocument, node, selector, update) {
     const items = sourceDocument.evaluate(
       `descendant::${selector}`,
       node,
@@ -41,19 +51,47 @@ var i18n = {
     }
   },
 
-  updateSubtree(sourceDocument, node) {
-    this.updateSubtreeSet(sourceDocument, node, `text()[contains(self::text(), "${this.keyPrefix}")]`, (text) => {
-      if (text.nodeValue.includes(this.keyPrefix))
-        text.nodeValue = this.updateString(text.nodeValue);
+  updateSubtree (sourceDocument, node) {
+    const { keyPrefix, updateSubtreeSet, i18nAttrRegex, propertyMapping } = this;
+
+    // Update element properties (including textContent) or attributes based on data-i18n-* attributes assigned to the element
+    updateSubtreeSet(sourceDocument, node, `*/@*[starts-with(name(), "data-i18n-")][contains(., "${keyPrefix}")]`, (attr) => {
+      const placeholder = attr.value;
+
+      if (placeholder.includes(keyPrefix)) {
+        const value = this.getTranslation(placeholder);
+        const { ownerElement } = attr;
+        let { target } = i18nAttrRegex.exec(attr.name).groups;
+
+        if(propertyMapping.has(target)) {
+          target = propertyMapping.get(target);
+        }
+
+        // If the target member is a property...
+        if(ownerElement[target] !== undefined) {
+          ownerElement[target] = value;
+        }
+        // Otherwise, assume it is an attribute
+        else {
+          ownerElement.setAttribute(target, value);
+        }
+      }
     });
 
-    this.updateSubtreeSet(sourceDocument, node, `*/attribute::*[contains(., "${this.keyPrefix}")]`, (attr) => {
-      if (attr.value.includes(this.keyPrefix))
-        attr.value = this.updateString(attr.value);
+    // Update text nodes containing __MSG_*__ placeholders
+    updateSubtreeSet(sourceDocument, node, `text()[contains(self::text(), "${keyPrefix}")]`, (text) => {
+      if (text.nodeValue.includes(keyPrefix))
+        text.nodeValue = this.getTranslation(text.nodeValue);
+    });
+
+    // Update element attributes (excluding data-i18n-*) containing __MSG_*__ placeholders
+    updateSubtreeSet(sourceDocument, node, `*/@*[not(starts-with(name(), "data-i18n-"))][contains(., "${keyPrefix}")]`, (attr) => {
+      if (attr.value.includes(keyPrefix))
+        attr.value = this.getTranslation(attr.value);
     });
   },
 
-  updateAnyDocument(sourceDocument, options = {}) {
+  updateAnyDocument (sourceDocument, options = {}) {
     this.extension = null;
     this.keyPrefix = "__MSG_";
   
@@ -65,7 +103,7 @@ var i18n = {
     this.updateSubtree(sourceDocument, sourceDocument);
   },
 
-  updateDocument(options = {}) {
+  updateDocument (options = {}) {
     this.updateAnyDocument(document, options);
-  },
+  }
 };

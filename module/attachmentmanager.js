af72ec0e38237cc5297b7423688400a4c1c2e43e
tmccoid.tech <coid.tpmc@gmail.com>
2026-01-16 17:26:46 -0800

    Temporary fix for encoded attachment filenames

--- module/attachmentmanager.js
+++ module/attachmentmanager.js
@@ -1867,10 +1867,28 @@ export class AttachmentManager {
     }
 
     #normalizeFileName(filename) {
+        let result = filename.trim();
+
+//        const { groups } = /^=\?utf-8\?B\?(?<b64>[A-Za-z0-9+/-_=]{4,})\?=/i.exec(filename);
+        const { groups } = /^=\?utf-8\?B\?(?<b64>[a-z0-9=+/_\-]{4,})\?=/i.exec(result) | {};
+
+        if(groups && groups.b64) {
+            const options = {};
+
+            if(/[_\-]+/.test(result)) {
+                options.alphabet = "base64url";
+            }
+
+            try {
+                result = new TextDecoder().decode(Uint8Array.fromBase64(groups.b64, options));
+            }
+            catch {
+                // Silently ignore
+            }
+        }
+
         const windowsForbiddenCharacterRegex = /[<>:"|?*\/\\]/g;
         const trailingDataDelimiterRegex = /.+?(?=;(?!.*\.+)|$)/;
-
-        let result = filename.trim();
 
         const trailingDataFilter = result.match(trailingDataDelimiterRegex);
 
